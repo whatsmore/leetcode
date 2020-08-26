fd97977717322dd4f7cd5592f570cb4653aa09e2
王晨沐 <195725796@qq.com>
2020-08-26 09:33:47 +0800

    20200826

--- src/main/java/com/wcm/tools/leetcode.java
+++ src/main/java/com/wcm/tools/leetcode.java
@@ -8,6 +8,50 @@ public class leetcode {
 
     }
 
+    /* leetcode 17. 电话号码的字母组合
+
+     */
+
+    /*leetcode 491. 递增子序列：给定一个整型数组, 找到所有该数组的递增子序列，递增子序列的长度至少是2。
+                    说明：给定数组的长度不会超过15。数组中的整数范围是 [-100,100]。给定数组中可能包含重复数字，相等的数字应该被视为递增的一种情况。
+                    测试用例：
+                        System.out.println(findSubsequences(new int[]{4,6,7,7}));
+                        System.out.println(findSubsequences(new int[]{1, 2, 2, 3}));
+                        System.out.println(findSubsequences(new int[]{1, 2, 1, 2, 3}));
+                        System.out.println(findSubsequences(new int[]{}));
+     */
+//    static List<List<Integer>> res = new ArrayList<>();
+//
+//    static List<List<Integer>> findSubsequences(int[] nums) {
+//        // idx 初始化为 -1，开始 dfs 搜索。
+//        dfs(nums, -1, new ArrayList<>());
+//        return res;
+//    }
+//
+//    static void dfs(int[] nums, int idx, List<Integer> curList) {
+//        // 只要当前的递增序列长度大于 1，就加入到结果 res 中，然后继续搜索递增序列的下一个值。
+//        if (curList.size() > 1) {
+//            res.add(new ArrayList<>(curList));
+//        }
+//
+//        // 在 [idx + 1, nums.length - 1] 范围内遍历搜索递增序列的下一个值。
+//        // 借助 set 对 [idx + 1, nums.length - 1] 范围内的数去重。
+//        Set<Integer> set = new HashSet<>();
+//        for (int i = idx + 1; i < nums.length; i++) {
+//            // 1. 如果 set 中已经有与 nums[i] 相同的值了，说明加上 nums[i] 后的所有可能的递增序列之前已经被搜过一遍了，因此停止继续搜索。
+//            if (set.contains(nums[i])) {
+//                continue;
+//            }
+//            set.add(nums[i]);
+//            // 2. 如果 nums[i] >= nums[idx] 的话，说明出现了新的递增序列，因此继续 dfs 搜索（因为 curList 在这里是复用的，因此别忘了 remove 哦）
+//            if (idx == -1 || nums[i] >= nums[idx]) {
+//                curList.add(nums[i]);
+//                dfs(nums, i, curList);
+//                curList.remove(curList.size() - 1);
+//            }
+//        }
+//    }
+
     /*leetcode 459. 重复的子字符串
             注意寻找规律！！！
             测试用例：
